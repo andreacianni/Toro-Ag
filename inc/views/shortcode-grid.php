71ea548f169820dac70b6cde4e9881dfc1c21de7
andreacianni <andrea.cianni@gmail.com>
2025-06-02 09:45:52 +0200

    Fix Griglia Applicazioni

--- inc/views/shortcode-grid.php
+++ inc/views/shortcode-grid.php
@@ -4,8 +4,8 @@ $img_field = get_query_var('toro_ag_grid_image_field','featured');
 ?>
 
 
-<?php if ( is_singular('prodotto') ) : // solo in singolo prodotto ?>
-  <h5 class="text-bg-dark px-3 py-2 my-4 rounded-2">Applicazioni</h5>
+<?php if ( is_singular('prodotto') && !empty($items) ) : // solo in singolo prodotto e se ci sono elementi ?>
+  <h5 class="text-bg-dark px-3 py-2 my-4 rounded-2"><?php _e( 'Applicazioni', 'toro-ag' ); ?></h5>
 <?php endif; ?>
 
 <div class="toro-grid <?php echo esc_attr( get_query_var('toro_ag_grid_wrapper_class','') ); ?>">
@@ -17,18 +17,18 @@ $img_field = get_query_var('toro_ag_grid_image_field','featured');
                 $title  = $item->name;
                 $link   = get_term_link($item);
             } else {
-                $img_id = ($img_field === 'featured')
+                $img_id = has_post_thumbnail($item->ID)
                     ? get_post_thumbnail_id($item->ID)
                     : get_post_meta($item->ID, $img_field, true);
                 $title  = get_the_title($item);
                 $link   = get_permalink($item);
             }
             if ($link) echo "<a class=\"d-block w-100\" href='" . esc_url($link) . "'>";
-            echo wp_get_attachment_image($img_id,'medium', false,['class' => 'd-block w-100 img-fluid', 'alt' => esc_attr($title)]);
+            echo wp_get_attachment_image($img_id, 'medium', false, ['class' => 'd-block w-100 img-fluid', 'alt' => esc_attr($title)]);
             if ($link) echo "</a>";
             ?>
             <?php if ($link) echo "<a href='" . esc_url($link) . "'>"; ?>
-                <h4 class="pb-0" ><?php echo esc_html($title); ?></h4>
+                <h4 class="pb-0"><?php echo esc_html($title); ?></h4>
             <?php if ($link) echo "</a>"; ?>
         </div>
     <?php endforeach; ?>
