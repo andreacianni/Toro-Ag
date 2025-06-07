1907c4fdbcec090abb9a187ccdd2982fdf0b4fd3
andreacianni <andrea.cianni@gmail.com>
2025-06-07 15:35:04 +0200

    modifica vista documenti downlaod rimossa vista table + card alpha 4

--- inc/views/documenti-download.php
+++ inc/views/documenti-download.php
@@ -125,10 +125,10 @@ $terms_data = array_slice( $terms_data, 0, 2 );
 <?php elseif ( $layout === 'card' ): ?>
 <ul class="list-group mb-5 documenti-download-list">
   <li class="list-group-item list-group-item-dark d-flex fw-bold">
-    <div class="col-4 ps-0"><?= esc_html__( 'Prodotti', 'toro-ag' ) ?></div>
-    <div class="col-2"><?= esc_html__( 'Lingua', 'toro-ag' ) ?></div>
-    <div class="col-3"><?= esc_html__( 'Schede', 'toro-ag' ) ?></div>
-    <div class="col-3 pe-0"><?= esc_html__( 'Documenti', 'toro-ag' ) ?></div>
+    <div class="col-3 ps-0"><?= esc_html__( 'Prodotti', 'toro-ag' ) ?></div>
+    <div class="col-1"><?= esc_html__( 'Lingua', 'toro-ag' ) ?></div>
+    <div class="col-4"><?= esc_html__( 'Schede', 'toro-ag' ) ?></div>
+    <div class="col-4 pe-0"><?= esc_html__( 'Documenti', 'toro-ag' ) ?></div>
   </li>
   <?php foreach ( $term['products'] as $prod ): ?>
     <?php
@@ -142,36 +142,34 @@ $terms_data = array_slice( $terms_data, 0, 2 );
     ?>
     <?php foreach ( $groups as $lang_slug => $data ): ?>
       <li class="list-group-item d-flex align-items-start gruppo-lingua-<?= esc_attr( $lang_slug ) ?>">
-        <div class="col-4 ps-0">
+        <div class="col-3 ps-0">
           <a href="<?= esc_url( get_permalink( $prod['ID'] ) ) ?>" class="prod-link d-block">
             <?= esc_html( $prod['title'] ) ?>
           </a>
         </div>
-        <div class="col-2">
+        <div class="col-1 d-flex align-items-center">
           <a href="<?= esc_url( get_permalink( $prod['ID'] ) ) ?>#" class="d-inline-block">
             <?= toroag_get_flag_html( $lang_slug ) ?>
           </a>
         </div>
-        <div class="col-3">
+        <div class="col-4">
           <ul class="list-unstyled mb-0 ps-0">
             <?php foreach ( $data['schede'] ?? [] as $sc ): ?>
-              <li class="d-flex align-items-center">
+              <li class="d-flex align-items-center mb-1">
                 <a href="<?= esc_url( $sc['url'] ) ?>" target="_blank" class="d-flex align-items-center">
-                  <?= toroag_get_flag_html( $lang_slug ) ?>
-                  <i class="bi <?= esc_attr( toroag_get_icon_class( $sc['url'] ) ) ?> ms-1 me-2"></i>
+                  <i class="bi <?= esc_attr( toroag_get_icon_class( $sc['url'] ) ) ?> me-2"></i>
                   <span><?= esc_html( $sc['title'] ) ?></span>
                 </a>
               </li>
             <?php endforeach; ?>
           </ul>
         </div>
-        <div class="col-3 pe-0">
+        <div class="col-4 pe-0">
           <ul class="list-unstyled mb-0 ps-0">
             <?php foreach ( $data['docs'] ?? [] as $doc ): ?>
-              <li class="d-flex align-items-center">
+              <li class="d-flex align-items-center mb-1">
                 <a href="<?= esc_url( $doc['url'] ) ?>" target="_blank" class="d-flex align-items-center">
-                  <?= toroag_get_flag_html( $lang_slug ) ?>
-                  <i class="bi <?= esc_attr( toroag_get_icon_class( $doc['url'] ) ) ?> ms-1 me-2"></i>
+                  <i class="bi <?= esc_attr( toroag_get_icon_class( $doc['url'] ) ) ?> me-2"></i>
                   <span><?= esc_html( $doc['title'] ) ?></span>
                 </a>
               </li>
@@ -182,60 +180,5 @@ $terms_data = array_slice( $terms_data, 0, 2 );
     <?php endforeach; ?>
   <?php endforeach; ?>
 </ul>
-<?php else: /* table layout */ ?>
-<div class="table-responsive mb-5 documenti-download-table">
-  <div class="row fw-bold border-bottom py-2 gx-2">
-    <div class="col-4"><?= esc_html__( 'Prodotti', 'toro-ag' ) ?></div>
-    <div class="col-2"><?= esc_html__( 'Lingua', 'toro-ag' ) ?></div>
-    <div class="col-3"><?= esc_html__( 'Schede', 'toro-ag' ) ?></div>
-    <div class="col-3"><?= esc_html__( 'Documenti', 'toro-ag' ) ?></div>
-  </div>
-  <div id="items-container">
-    <?php foreach ( $term['products'] as $prod ): ?>
-      <?php
-        $groups = [];
-        foreach ( $prod['schede'] as $item ) {
-          $groups[ $item['lang'] ]['schede'][] = $item;
-        }
-        foreach ( $prod['docs'] as $item ) {
-          $groups[ $item['lang'] ]['docs'][] = $item;
-        }
-      ?>
-      <?php foreach ( $groups as $lang_slug => $data ): ?>
-        <div class="row align-items-start py-2 gx-2 gruppo-lingua-<?= esc_attr( $lang_slug ) ?>">
-          <div class="col-4 d-flex align-items-center">
-            <a href="<?= esc_url( get_permalink( $prod['ID'] ) ) ?>"><?= esc_html( $prod['title'] ) ?></a>
-          </div>
-          <div class="col-2 d-flex align-items-center">
-            <?= toroag_get_flag_html( $lang_slug ) ?>
-          </div>
-          <div class="col-3">
-            <ul class="list-unstyled mb-0">
-              <?php foreach ( $data['schede'] ?? [] as $sc ): ?>
-                <li class="d-flex align-items-center">
-                  <span class="icone me-1">
-                    <i class="bi <?= esc_attr( toroag_get_icon_class( $sc['url'] ) ) ?>"></i>
-                  </span>
-                  <a href="<?= esc_url( $sc['url'] ) ?>" target="_blank"><?= esc_html( $sc['title'] ) ?></a>
-                </li>
-              <?php endforeach; ?>
-            </ul>
-          </div>
-          <div class="col-3">
-            <ul class="list-unstyled mb-0">
-              <?php foreach ( $data['docs'] ?? [] as $doc ): ?>
-                <li class="d-flex align-items-center">
-                  <span class="icone me-1">
-                    <i class="bi <?= esc_attr( toroag_get_icon_class( $doc['url'] ) ) ?>"></i>
-                  </span>
-                  <a href="<?= esc_url( $doc['url'] ) ?>" target="_blank"><?= esc_html( $doc['title'] ) ?></a>
-                </li>
-              <?php endforeach; ?>
-            </ul>
-          </div>
-        </div>
-      <?php endforeach; ?>
-    <?php endforeach; ?>
-  </div>
-</div>
-<?php endif; ?><?php endforeach; ?>
+<?php endif; ?>
+<?php endforeach; ?>
