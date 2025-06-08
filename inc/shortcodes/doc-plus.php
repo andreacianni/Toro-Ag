ce2d450997b1563db1d41b53a8e7819c19027393
andreacianni <andrea.cianni@gmail.com>
2025-06-08 14:40:46 +0200

    doc_plus_debug: shortcode eseguito V3

--- inc/shortcodes/doc-plus.php
+++ inc/shortcodes/doc-plus.php
@@ -5,7 +5,7 @@
  */
 function doc_plus_debug_shortcode() {
     // COMMENTO DI CONTROLLO: serve a verificare se lo shortcode viene eseguito
-    $out = "\n<!-- doc_plus_debug: shortcode eseguito V2 -->\n";
+    $out = "\n<!-- doc_plus_debug: shortcode eseguito V3 -->\n";
 
     $page_id    = get_the_ID();
     $page_pod   = pods( 'page', $page_id );
