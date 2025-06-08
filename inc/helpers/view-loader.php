f813952e962c0579b10d7052e926e3f8a9015447
andreacianni <andrea.cianni@gmail.com>
2025-06-08 22:48:21 +0200

    DOC+ VIEWS cases debug 3

--- inc/helpers/view-loader.php
+++ inc/helpers/view-loader.php
@@ -19,10 +19,18 @@ if ( ! function_exists( 'toroag_load_view' ) ) {
             return '<p>View "' . esc_html( $view ) . '" non trovata.</p>';
         }
 
+        // Debug: log dei dati forniti al loader
+        echo '<!-- Debug loader BEFORE extract: ' . esc_html( wp_json_encode( $data ) ) . ' -->';
+
+        // Estraiamo le variabili dal data array
         extract( $data, EXTR_SKIP );
+
+        // Debug: log delle variabili estratte (layout)
+        $layout_debug = isset( $layout ) ? $layout : 'undefined';
+        echo '<!-- Debug loader AFTER extract: layout = ' . esc_html( $layout_debug ) . ' -->';
+
         ob_start();
-        include $view_file;
-        var_dump($data);
+        include $view_file;  // all'interno di questo file, dovremmo avere accesso a $layout e $doc_plus_data
         return ob_get_clean();
     }
 }
