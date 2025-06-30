ab20c876039d9d19a5311a73570a0bef948439f5
andreacianni <andrea.cianni@gmail.com>
2025-06-30 09:29:55 +0200

    classi titoletti doc-plus e video card

--- inc/shortcodes/video-card.php
+++ inc/shortcodes/video-card.php
@@ -252,7 +252,7 @@ function ac_video_pagina_shortcode($atts = []) {
     ob_start();
 
     if (! empty($atts['titolo'])) {
-        echo '<h5 class="text-bg-dark text-center py-2 my-4 rounded-2">' . esc_html($atts['titolo']) . '</h5>';
+        echo '<h5 class="text-start fw-bold border-bottom ps-1 py-2 my-4">' . esc_html($atts['titolo']) . '</h5>';
     }
 
     echo '<div id="video-pagina-wrapper">';
