b82803b100be12d60b1545622db367b3fc49dbcd
andreacianni <andrea.cianni@gmail.com>
2025-07-18 17:38:45 +0200

    rounting email in un plugin

--- functions.php
+++ functions.php
@@ -67,21 +67,6 @@ function aggiungi_bootstrap_admin() {
 }
 add_action('admin_enqueue_scripts', 'aggiungi_bootstrap_admin');
 
-// Carica il file per il routing delle email
-require_once get_stylesheet_directory() . '/inc/cf7-email-routing.php';
-
-// Carica script privacy toggle per form prodotti
-function enqueue_privacy_toggle_script() {
-    wp_enqueue_script(
-        'privacy-toggle',
-        get_stylesheet_directory_uri() . '/assets/js/privacy-toggle.js',
-        [],
-        '1.0.0',
-        true
-    );
-}
-add_action('wp_enqueue_scripts', 'enqueue_privacy_toggle_script');
-
 
 // carica helpers e shortcodes
 require_once get_stylesheet_directory() . '/inc/helpers/file-icon.php';
